6b7305d4f9224b32497e3ecb93474dcea4229edd
John Daniel <daniel@bosser.com>
2013-08-27 20:11:11 +0200

    everything working as it should once more

--- src/com/johndaniel/glosar/StartPoint.java
+++ src/com/johndaniel/glosar/StartPoint.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.FrameLayout;
 import android.app.Activity;
+import android.content.Intent;
 
 
 public class StartPoint extends SherlockFragmentActivity implements ListOfFilesFragment.OnTrainingSelectedListener {
@@ -21,12 +22,12 @@ public class StartPoint extends SherlockFragmentActivity implements ListOfFilesF
 		setContentView(R.layout.activity_start_point);
 		
 		boolean isTablet = getResources().getBoolean(R.bool.isTablet);
-		if(!isTablet){
+		if(isTablet){ //We are on a TABLET, folks!!
 			FrameLayout container = (FrameLayout) findViewById(R.id.start_point_container);
 			FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
 			
 			fm.add(R.id.start_point_container, new ListOfFilesFragment()).commit();
-		} else { //We are on a TABLET, folks!!
+		} else { 
 			FrameLayout container = (FrameLayout) findViewById(R.id.start_point_container);
 			FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
 			
@@ -40,23 +41,45 @@ public class StartPoint extends SherlockFragmentActivity implements ListOfFilesF
 		getSupportMenuInflater().inflate(R.menu.start_point, menu);
 		return true;
 	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()){
+		case R.id.addNewFileButton: 
+			Intent addIntent = new Intent(this, addFile.class);
+			startActivity(addIntent);
+			return true;
+		case R.id.action_settings:
+			Intent settingsIntent = new Intent(this, SettingsActivity.class);
+			startActivity(settingsIntent);
+			return true;
+		default: return super.onOptionsItemSelected(item);
+		}
+	}
 
 	@Override
 	public void showTraining(String chosenTraining) {
 		// TODO Auto-generated method stub
 		boolean isTablet = getResources().getBoolean(R.bool.isTablet);
-		if (!isTablet){
+		if (isTablet){
+			
+			
+		} else { //On tablet: should replace the fragment at the right side
 			OverviewFragment overviewFragment = new OverviewFragment();
 			Bundle args = new Bundle();
 			args.putString("TRAINING", chosenTraining);
 			overviewFragment.setArguments(args);
 			
 			FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
-			fm.replace(R.id.start_point_container, overviewFragment);
-			
-		} else { //On tablet: should replace the fragment at the right side
-			
+			fm
+			//.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+			.replace(R.id.start_point_container, overviewFragment)
+			.addToBackStack(null)
+			.commit();
 		}
 	}
+	
 
 }
